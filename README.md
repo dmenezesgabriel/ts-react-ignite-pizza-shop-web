9a42231a9d8126e60ea2be3f6f4a2449de628fef
Gabriel Menezes <dmenezes.gabriel@gmail.com>
2024-12-22 12:08:19 -0300

    chore: setup app

---
+++ README.md
@@ -0,0 +1,8 @@
+# Pizza Shop Frontend
+
+## Resources
+
+- [chadcn](https://ui.shadcn.com/)
+- [eslint-plugin-simple-import-sort](https://github.com/lydell/eslint-plugin-simple-import-sort)
+- [prettier-plugin-tailwindcss](https://github.com/tailwindlabs/prettier-plugin-tailwindcss)
+- [reactrouter](https://reactrouter.com/)
---
+++ src/App.tsx
@@ -0,0 +1,27 @@
+import "./index.css";
+
+import { Helmet, HelmetProvider } from "react-helmet-async";
+import { BrowserRouter, Route, Routes } from "react-router";
+
+import { AppLayout } from "./pages/_layouts/app";
+import { AuthLayout } from "./pages/_layouts/auth";
+import { Dashboard } from "./pages/app/dashboard";
+import { SignIn } from "./pages/auth/sign-in";
+
+export function App() {
+  return (
+    <HelmetProvider>
+      <Helmet titleTemplate="%s | pizza.shop" />
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<AppLayout />}>
+            <Route path="/" element={<Dashboard />} />
+          </Route>
+          <Route path="/" element={<AuthLayout />}>
+            <Route path="/sign-in" element={<SignIn />} />
+          </Route>
+        </Routes>
+      </BrowserRouter>
+    </HelmetProvider>
+  );
+}
