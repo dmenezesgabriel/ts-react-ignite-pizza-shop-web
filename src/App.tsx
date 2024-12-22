505220e89ecdd785c7fea00c18ac5d187762f2cf
Gabriel Menezes <dmenezes.gabriel@gmail.com>
2024-12-22 18:54:11 -0300

    feat: add header

--- src/App.tsx
+++ src/App.tsx
@@ -4,26 +4,31 @@ import { Helmet, HelmetProvider } from "react-helmet-async";
 import { BrowserRouter, Route, Routes } from "react-router";
 import { Toaster } from "sonner";
 
+import { ThemeProvider } from "./components/theme/theme-provider";
 import { AppLayout } from "./pages/_layouts/app";
 import { AuthLayout } from "./pages/_layouts/auth";
 import { Dashboard } from "./pages/app/dashboard";
 import { SignIn } from "./pages/auth/sign-in";
+import { SignUp } from "./pages/auth/sign-up";
 
 export function App() {
   return (
     <HelmetProvider>
-      <Helmet titleTemplate="%s | pizza.shop" />
-      <Toaster richColors />
-      <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<AppLayout />}>
-            <Route path="/" element={<Dashboard />} />
-          </Route>
-          <Route path="/" element={<AuthLayout />}>
-            <Route path="/sign-in" element={<SignIn />} />
-          </Route>
-        </Routes>
-      </BrowserRouter>
+      <ThemeProvider storageKey="pizza-shop-theme" defaultTheme="dark">
+        <Helmet titleTemplate="%s | pizza.shop" />
+        <Toaster richColors />
+        <BrowserRouter>
+          <Routes>
+            <Route path="/" element={<AppLayout />}>
+              <Route path="/" element={<Dashboard />} />
+            </Route>
+            <Route path="/" element={<AuthLayout />}>
+              <Route path="/sign-in" element={<SignIn />} />
+              <Route path="/sign-up" element={<SignUp />} />
+            </Route>
+          </Routes>
+        </BrowserRouter>
+      </ThemeProvider>
     </HelmetProvider>
   );
 }
