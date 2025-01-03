5e406bb61c603ce46a1d4ff0287ab310be6bf3a6
Gabriel Menezes <dmenezes.gabriel@gmail.com>
2025-01-02 22:29:01 -0300

    feat: add dialog

--- src/pages/app/orders/order-table-row.tsx
+++ src/pages/app/orders/order-table-row.tsx
@@ -1,16 +1,27 @@
 import { ArrowRight, Search, X } from "lucide-react";
 
 import { Button } from "../../../components/ui/button";
+import {
+  Dialog,
+  DialogContent,
+  DialogTrigger,
+} from "../../../components/ui/dialog";
 import { TableCell, TableRow } from "../../../components/ui/table";
 
 export function OrderTableRow() {
   return (
     <TableRow>
       <TableCell>
-        <Button variant="outline" size="xs">
-          <Search className="h-3 w-3" />
-          <span className="sr-only">Detalhes do pedido </span>
-        </Button>
+        <Dialog>
+          <DialogTrigger asChild>
+            <Button variant="outline" size="xs">
+              <Search className="h-3 w-3" />
+              <span className="sr-only">Detalhes do pedido </span>
+            </Button>
+          </DialogTrigger>
+
+          <DialogContent>Teste</DialogContent>
+        </Dialog>
       </TableCell>
       <TableCell className="font-mono text-xs font-medium">
         12j4g2uy35123j23j12n
