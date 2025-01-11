a291742875168e6044732289d338d54ebdec3fc3
Gabriel Menezes <dmenezes.gabriel@gmail.com>
2025-01-11 13:13:50 -0300

    refactor: refactor react router

--- src/App.tsx
+++ src/App.tsx
@@ -2,18 +2,12 @@ import "./index.css";
 
 import { QueryClientProvider } from "@tanstack/react-query";
 import { Helmet, HelmetProvider } from "react-helmet-async";
-import { BrowserRouter, Route, Routes } from "react-router";
+import { RouterProvider } from "react-router";
 import { Toaster } from "sonner";
 
 import { ThemeProvider } from "./components/theme/theme-provider";
 import { queryClient } from "./lib/react-query";
-import { AppLayout } from "./pages/_layouts/app";
-import { AuthLayout } from "./pages/_layouts/auth";
-import { NotFound } from "./pages/404";
-import { Dashboard } from "./pages/app/dashboard/dashboard";
-import { Orders } from "./pages/app/orders/orders";
-import { SignIn } from "./pages/auth/sign-in";
-import { SignUp } from "./pages/auth/sign-up";
+import { router } from "./routes";
 
 export function App() {
   return (
@@ -22,19 +16,7 @@ export function App() {
         <Helmet titleTemplate="%s | pizza.shop" />
         <Toaster richColors />
         <QueryClientProvider client={queryClient}>
-          <BrowserRouter>
-            <Routes>
-              <Route path="/" element={<AppLayout />}>
-                <Route path="/" element={<Dashboard />} />
-                <Route path="/orders" element={<Orders />} />
-                <Route path="*" element={<NotFound />} />
-              </Route>
-              <Route path="/" element={<AuthLayout />}>
-                <Route path="/sign-in" element={<SignIn />} />
-                <Route path="/sign-up" element={<SignUp />} />
-              </Route>
-            </Routes>
-          </BrowserRouter>
+          <RouterProvider router={router} />
         </QueryClientProvider>
       </ThemeProvider>
     </HelmetProvider>
