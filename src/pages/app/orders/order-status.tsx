e45c432b3b6d98949657e78f678aa21deeb32dc0
Gabriel Menezes <dmenezes.gabriel@gmail.com>
2025-01-11 21:23:00 -0300

    tests: add order status unit tests

--- src/pages/app/orders/order-status.tsx
+++ src/pages/app/orders/order-status.tsx
@@ -22,25 +22,25 @@ export function OrderStatus({ status }: OrderStatusProps) {
     <div className="flex items-center gap-2">
       {status === "pending" && (
         <span
-          data-test-id="badge"
+          data-testid="badge"
           className="h-2 w-2 rounded-full bg-slate-400"
         ></span>
       )}
       {status === "canceled" && (
         <span
-          data-test-id="badge"
+          data-testid="badge"
           className="h-2 w-2 rounded-full bg-rose-500"
         ></span>
       )}
       {status === "delivered" && (
         <span
-          data-test-id="badge"
+          data-testid="badge"
           className="h-2 w-2 rounded-full bg-emerald-500"
         ></span>
       )}
       {["processing", "delivering"].includes(status) && (
         <span
-          data-test-id="badge"
+          data-testid="badge"
           className="h-2 w-2 rounded-full bg-amber-400"
         ></span>
       )}
