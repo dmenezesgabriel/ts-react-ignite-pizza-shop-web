a44498fd7797bed5e8b5f3c955c2fb6358d15dfc
Gabriel Menezes <dmenezes.gabriel@gmail.com>
2025-01-12 20:00:48 -0300

    tests: add profile mock

---
+++ src/api/mocks/get-managed-restaurant-mock.ts
@@ -0,0 +1,18 @@
+import { http, HttpResponse } from "msw";
+
+import { GetManagedRestaurantResponse } from "../get-managed-restaurant";
+
+export const GetManagedRestaurantMock = http.get<
+  never,
+  never,
+  GetManagedRestaurantResponse
+>("/managed-restaurant", () => {
+  return HttpResponse.json({
+    id: "custom-restaurant-id",
+    managerId: "custom-user-id",
+    name: "Pizza Shop",
+    description: "Custom restaurant description.",
+    createdAt: new Date(),
+    updatedAt: null,
+  });
+});
